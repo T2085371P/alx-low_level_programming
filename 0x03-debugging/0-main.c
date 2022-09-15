2c58a9fb1d986bb12cfdaff9d1f95b25905539b7
T2085371P <vimbaimapfumo25@gmail.com>
2022-09-15 04:50:43 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-
  * main - prints positive or negative integer
  * Return: 0
  */
 
 int main(void)
-
 {
 	int i;
-        i = 0;
+
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 
