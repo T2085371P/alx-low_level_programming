e5d4e0f2da64241046cc6be676840f9ca0176f9a
T2085371P <vimbaimapfumo25@gmail.com>
2022-09-28 14:39:25 -0700

    Task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,3 @@
-fndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
 int _putchar(char);
 void _puts_recursion(char *);
 void _print_rev_recursion(char *);
@@ -11,5 +8,3 @@ int _sqrt_recursion(int);
 int is_prime_number(int);
 int is_palindrome(char *);
 int wildcmp(char *, char *);
-
-#endif
